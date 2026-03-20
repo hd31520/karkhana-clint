f2d31db91f04bf2c9717d80c0b98717843614d21
hd31520 <mdhridoy3240@gmail.com>
2026-03-20 06:03:09 +0600

    change online server

--- vite.config.js
+++ vite.config.js
@@ -50,7 +50,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://kharkhana-server.vercel.app',
         changeOrigin: true,
       },
     },
